03f97421a4ce22eb03cb3b48abc6f575c42cb81f
jakub kapitulcin <kapitulcinjakub1@gmail.com>
2020-10-04 13:55:36 +0200

    Aha

--- Open-Lab-02.01/Exercise.cs
+++ Open-Lab-02.01/Exercise.cs
@@ -6,12 +6,12 @@ namespace Open_Lab_02._01
     {
         public double[] Create5Numbers()
         {
-            throw new NotImplementedException();
+            return new double[5];
         }
 
         public double GetSecond(double[] numbers)
         {
-            throw new NotImplementedException();
+            return numbers[1];
         }
     }
 }
